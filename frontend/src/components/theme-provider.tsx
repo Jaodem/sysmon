f2ac2f8c24f08e9ca03016e49586db60548e23fc
Jaodem <orellana.joseaugusto@gmail.com>
2026-05-14 21:10:46 -0300

    feat: agregar ThemeProvider con next-themes

---
+++ frontend/src/components/theme-provider.tsx
@@ -0,0 +1,20 @@
+// Proveedor de tema que envuelve la aplicación y gestiona el tema claro/oscuro
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
